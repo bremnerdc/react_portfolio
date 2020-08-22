2efb0a047a2a42f180149180a5b069f9ac2623a7
Dylan Bremner <61300825+bremnerdc@users.noreply.github.com>
2020-08-22 11:44:22 -0700

    added portoflio pics

---
+++ src/resumeData.js
@@ -0,0 +1,125 @@
+let resumeData = {
+    "imagebaseurl":"https://bremnerdc.github.io/",
+    "name": "Dylan Bremner",
+    "role": "Junior Full-Stack Developer",
+    "linkedinId":"linkedin.com/in/dylanbremner",
+    "skypeid": "dylan.bremner@weyerhaeuser.com",
+    "roleDescription": "I'm a Technical Solutions Analyst at Weyerhaeuser, looking to break into the development field. I am more comfortable with front-end development, but I am open to back-end as well. I am a full time learner and I am constantly working on improving my knowledge and abilities in both IT and development.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/dylanbremner",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/bremnerdc",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/5GRoAT",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I just finished a Full-Stack Development bootcamp with an emphasis on JavaScript at the University of Washington, and I am currently diving deeper into other coding languages such as Python, C#, and C++. Everyday I am constantly learning new things and working towards my goal of becoming a full-time web developer.",
+    "address":"Seattle, WA",
+    "website":"https://bremnerdc.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Washington",
+        "specialization":"Javascript, React, HTML, CSS, SQL, Mongo",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Graduated with an A+ at the University of Washington Javascript Bootcamp"
+      },
+      // {
+      //   "UniversityName":"Cuesta College",
+      //   "specialization":"Associates of Arts",
+      //   "MonthOfPassing":"Aug",
+      //   "YearOfPassing":"2012",
+      //   "Achievements":"Graduated with an Associates in Arts degree at Cuesta College."
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"Weyerhaeuser",
+        "specialization":"Technical Solutions Analyst II",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"2020",
+        "Achievements":"I lead a small, dedicated team that provides support to the employees at Weyerhaeuser HQ."
+      },
+      {
+        "CompanyName":"Premera Blue Cross",
+        "specialization":"Life Cycle Management Specialist",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2019",
+        "Achievements":"Provided support to all employees at the Mountlake Terrace Premera campus."
+      }
+    ],
+    "skillsDescription":"My skills:",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"JS"
+      },
+      {
+        "skillname":"JQuery"
+      },
+      {
+        "skillname":"Handlebars"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"NoSQL"
+      },
+      {
+        "skillname":"MERN"
+      }
+      
+    ],
+    "portfolio":[
+      {
+        "name":"Corona Connoisseur",
+        "description":"Responsive Web Application helping people support local businesses during the pandemic.",
+        "imgurl":"images/portfolio/cc.png"
+      },
+      {
+        "name":"Air-Share",
+        "description":"An E-Business Card website and mobile application that uses QR Codes to quickly exchange contact information and create a professional network.",
+        "imgurl":"images/portfolio/airshare.png"
+      },
+      {
+        "name":"Fitness Tracker",
+        "description":"A website that helps you track workouts and fitness goals.",  
+        "imgurl":"images/portfolio/FT.png"
+      },
+      {
+        "name":"Employee Tracker",
+        "description":"A command line interface (CLI) that allows you to keep track of employees.",
+        "imgurl":"images/portfolio/ET.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Dylan is a great person all around to work with - he is a hard worker and always willing to go the extra mile.",
+        "name":"Angel Koh - Project Manager at Premera"
+      },
+      {
+        "description":"Dylan was extremely helpful in getting my problem solved. I've had this issue for over two years and have worked with numerous people with no resolution. Dylan worked with me over the course of two days and he was able to locate the issue and come up with a solution.",
+        "name":"Juan Carlos - Distribution Manager at Weyerhaeuser"
+      }
+    ]
+  }
+  
+  export default resumeData
